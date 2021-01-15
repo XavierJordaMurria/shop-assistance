a9ceeff620fa0ee6035e19bc749846e637db8bfa
xavi <xavier@traffgen.com>
2021-01-15 07:39:51 +0100

    returning proper html fiels

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-    
+const path = require('path');
+const rootDir = require('./util/path');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 // if (url === '/upload') {
@@ -36,13 +38,10 @@ const { r, g, b, w, c, m, y, k } = [
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use('/', shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send(`
-  <h1>
-    Page not found! :(
-  </h1>`);
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(port);
