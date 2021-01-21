1389fbab60c917d0108db6e7dd901cf091ef70ac
xavier Jorda <jorda.murria.xavier@gmail.com>
2021-01-21 20:37:03 +0100

    adding admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,18 +11,19 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddproduct = (req, res) => {
-    const product = new Product(req.body.title)
+    const product = new Product(req.body.title, req.body.imageUrl, req.body.description, req.body.price);
     product.save();
     res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('admin/add-product', {
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+    Product.fetchAll((products) => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products',
+        });
     });
+
 };
 
