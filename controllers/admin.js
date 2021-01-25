7212d3cf3949546ed51138621e873ca6dcd91056
xavier Jorda <jorda.murria.xavier@gmail.com>
2021-01-25 09:26:43 +0100

    Edditing product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        editing: false
     });
 };
 
@@ -14,6 +12,29 @@ exports.postAddproduct = (req, res) => {
     const product = new Product(req.body.title, req.body.imageUrl, req.body.description, req.body.price);
     product.save();
     res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = Boolean(req.query.edit);
+    console.log(`EditProduct with editMode: ${editMode}`)
+
+    if (!editMode) {
+        return res.redirect('/shop');
+    }
+
+    const prodId = req.params.productId;
+    Product.findById(prodId, (product) => {
+        if (!product) {
+            console.error(`No product for id:${prodId}`);
+            return res.redirect('/shop');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    });
 };
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const adminController = require('../controllers/admin');
 
 router.get('/add-product', adminController.getAddProduct);
 router.post("/add-product", adminController.postAddproduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 router.get('/products', adminController.getProducts);
 
 module.exports = router;
