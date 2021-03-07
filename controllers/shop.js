4af5c94b1f4c78a59c3429c42be95b43cf0a01a2
xavier Jorda <jorda.murria.xavier@gmail.com>
2021-03-07 11:25:41 +0100

    making pagination button dynamic

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,18 +6,31 @@ const Product = require('../models/product');
 const Order = require('../models/orders');
 const orders = require('../models/orders');
 const { set } = require('mongoose');
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 
 exports.getProducts = (req, res, next) => {
-    Product.find({})
-        .then((products) => {
-            res.render('shop/product-list', {
-                prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
-            });
-        })
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.countDocuments().then(numDocs => {
+        totalItems = numDocs;
+        return Product.find({})
+        .skip((page -1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })    
+    .then((products) => {
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'Products',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE*page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page +1,
+            previousPage: page-1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+            path: '/products'
+        });
+    })
         .catch(e => {
             console.error(e);
             const error = new Error(e);
@@ -41,23 +54,33 @@ exports.getProductsById = (req, res, next) => {
 
 
 exports.getIndex = (req, res, next) => {
-    const page = req.query.page;
-    Product.find({})
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.countDocuments().then(numDocs => {
+        totalItems = numDocs;
+        return Product.find({})
         .skip((page -1) * ITEMS_PER_PAGE)
         .limit(ITEMS_PER_PAGE)
-        .then((products) => {
-            res.render('shop/index', {
-                prods: products,
-                pageTitle: 'Shop',
-                path: '/'
-            });
-        })
-        .catch(e => {
-            console.error(e);
-            const error = new Error(e);
-            error.httpStatusCode = 500;
-            return next(error);
-          });;
+    })    
+    .then((products) => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'Shop',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE*page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page +1,
+            previousPage: page-1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+            path: '/'
+        });
+    })
+    .catch(e => {
+        console.error(e);
+        const error = new Error(e);
+        error.httpStatusCode = 500;
+        return next(error);
+        });;
 };
 
 exports.getCart = (req, res, next) => {
